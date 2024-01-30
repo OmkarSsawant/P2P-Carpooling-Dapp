d39c1697c4881c7cb9f28c36e16f02b0df9c203b
OmkarSsawant <omkar18sawant@gmail.com>
2024-01-31 02:16:23 +0530

    Created Ride Done

--- src/components/create-ride.tsx
+++ src/components/create-ride.tsx
@@ -64,36 +64,34 @@ useEffect(() => {
 
   async function createRide() {
     
-    // let hash = await writeContractAsync({
-    //   abi,
-    //   address:"0x5FbDB2315678afecb367f032d93F642f64180aa3",
-    //   functionName:"registerRide",
-    //   args:[
-    //       (car.data as any).seats,
-    //       String(startPlace?.address?.freeformAddress),
-    //       String(endPlace?.address?.freeformAddress),
-    //       BigInt(parseFloat(fee) * 10**18),
-    //       Date.parse((dateTimeInput!.current! as any).value),
-    //       0
-    //   ]  
-    // });
-    // alert(`Ride Created ${hash}`)
+    let hash = await writeContractAsync({
+      abi,
+      address:"0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      functionName:"registerRide",
+      args:[
+          (car.data as any).seats,
+          String(startPlace?.address?.freeformAddress),
+          String(endPlace?.address?.freeformAddress),
+          BigInt(parseFloat(fee) * 10**18),
+          Date.parse((dateTimeInput!.current! as any).value),
+          0
+      ]  
+    });
+    alert(`Ride Created ${hash}`)
  await  fetch('/api/create-ride',{
     method:"POST",
     headers:{
       "Content-Type":"application/json"
     },
-    
     body:JSON.stringify({
-      "name" : "BigInt((car.data as any)[6])",
-      "start" : "startPlace",
-     "end" : "endPlace",
-     "fare": "BigInt(parseFloat(fee) * 10**18)",
-      "depart" : "Date.parse((dateTimeInput!.current! as any).value)",
+      "name" : BigInt((car.data as any)[6]),
+      "start" : startPlace,
+     "end" : endPlace,
+     "fare": BigInt(parseFloat(fee) * 10**18),
+      "depart" : Date.parse((dateTimeInput!.current! as any).value),
       "reached":0
     })
-  })
-  .catch(console.error)
+  }).catch(console.error)
 
 
   }
